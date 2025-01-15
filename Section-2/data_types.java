c838d556b3c3c886a7c2a686a9357a6841997617
Vrushabh Panchal <vrushabh071@gmail.com>
2025-01-16 00:09:04 +0530

    variables and data type done

---
+++ Section-2/data_types.java
@@ -0,0 +1,25 @@
+public class data_types {
+    public static void main(String[] args) {
+        
+        int a = 1;
+        byte by = 127;
+        short sh = 558;
+        long l = 5854l;
+
+        float f = 2.5f;
+        double d = 3.5;
+
+        char c = 'k';
+
+        boolean b = true;
+        System.out.println(a + " <========== INTEGER");
+        System.out.println(by + " <========== BYTE");
+        System.out.println(sh + " <========== SHORT");
+        System.out.println(l + " <========== LONG");
+        System.out.println(f + " <========== FLOAT");
+        System.out.println(d + " <========== DOUBLE");
+        System.out.println(c + " <========== CHARACTER");
+        System.out.println(b + " <========== BOOLEAN");
+        
+    }
+}
